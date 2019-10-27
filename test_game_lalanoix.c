3e71bfe749a863462227e0ec93f9516ace5080f7
jin eric <eric.jin@etu.u-bordeaux.fr>
2019-10-27 22:04:47 +0100

    les_modif_laura

--- test_game_lalanoix.c
+++ test_game_lalanoix.c
@@ -32,6 +32,10 @@ bool test_game_restart(){
   };
 
   game g= game_new(cell,SIZE);
+   if (g==NULL){
+    fprintf(stderr, "ERROR! \n");
+    return false;
+  }
 
   if(game_nb_moves_cur(g)!=0){
     fprintf(stderr, "Error:nb moves cur\n");
@@ -42,40 +46,59 @@ bool test_game_restart(){
     for (int y=0; y<SIZE; y++){
       unsigned int cellc =game_cell_current_color(g,x,y);
       if(cellc<0 || cellc>3){
-        fprintf(stderr, "Error: cell is empty!\n");
+        fprintf(stderr, "Error: cell problem\n");
         game_delete(g);
         return false;
       }
     }
   }
+
   game_delete(g);
   return true;
 }
 /* ********** TEST IS OVER ********** */
 
 bool test_game_is_over(){
-  game g= game_new_empty();
+  //game g= game_new_empty();
+
+  color cell[]= {
+      0,0,0,2,0,2,1,0,1,0,3,0,
+      0,3,3,1,1,1,1,3,2,0,1,0,
+      1,0,1,2,3,2,3,2,0,3,3,2,
+      2,3,1,0,3,2,1,1,1,2,2,0,
+      2,1,2,3,3,3,3,2,0,1,0,0,
+      0,3,3,0,1,1,2,3,3,2,1,3,
+      1,1,2,2,2,0,0,1,3,1,1,2,
+      1,3,1,3,1,0,1,0,1,3,3,3,
+      0,3,0,1,0,0,2,1,1,1,3,0,
+      1,3,1,0,0,0,3,2,3,1,0,0,
+      1,3,3,1,1,2,2,3,2,0,0,2,
+      2,0,2,3,0,1,1,1,2,3,0,1
+  };
+
+  game g= game_new(cell,SIZE);
+
   if (g==NULL){
     fprintf(stderr, "ERROR! \n");
     return false;
   }
 
-  unsigned int colorcell = game_cell_current_color(g,0,0);
 
   if(game_nb_moves_cur(g)>SIZE){
     fprintf(stderr, "Error!\n");
     return false;
   }
 
-  for (int x=0; x< SIZE-1;x++){
-    for (int y=0; y<SIZE-1; y++){
-      if(colorcell!=game_cell_current_color(g,x,y)){
-        fprintf(stderr, "Error!\n");
-        return false;
-      }
-    }
+  int sol[12] = {3,1,3,1,0,3,1,0,1,3,2,0};
+  for (int i=0; i<12;i++){
+    game_play_one_move(g, sol[i]);
   }
 
+  if(!game_is_over(g)){
+    fprintf(stderr, "Error 404!\n");
+    return false;
+    game_delete(g);
+  }
   game_delete(g);
   return true;
 }
@@ -89,8 +112,18 @@ bool test_game_delete(){
     fprintf(stderr, "ERROR! \n");
     return false;
   }
+  /*for (int x=0; x< SIZE-1;x++){
+    for (int y=0; y<SIZE-1; y++){
+      if (game_cell_current_color(g,x,y) == NULL){
+        fprintf(stderr, "Error");
+        return false;
+      }
+    }
+  } */
 
   game_delete(g);
+
+
   return true;
 }
 
