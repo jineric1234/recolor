2b96b00ae297a6c7faa633d4175df4f0dd778c2d
BOUMEDRIS Zineb <zineb.boumedris@etu.u-bordeaux.fr>
2019-11-19 21:31:17 +0100

    modification dans la structure  des fonctions game_new, game_new_empty, game_set_cell_init,game_set_max_moves,game_nb_moves_max,game_cell_current_color,game_nb_moves_cur

--- game.c
+++ game.c
@@ -41,66 +41,115 @@ color cell[]= {
 
 typedef unsigned int uint;
 
-typedef enum color_e {
+/* typedef enum color_e {
     RED,
     GREEN,
     BLUE,
     YELLOW,
     NB_COLORS
-} color;
+} color; */
 
-typedef struct game_s {
+struct game_s {
     color *cell; //pointe vers la grille du jeu
-    color *cell_init //pointe vers la grille du jeu initiale
+    color *cell_init; //pointe vers la grille du jeu initiale
     uint nbmax; // nombre max de move
     uint nbmovecur; //nombre de mouvements actuel
-}game;
+};
 
 typedef const struct game_s *cgame;
 
 
 game game_new(color *cells, uint nb_moves_max){
-    return NULL;
-}
-
-
-game game_new_empty(){
-    return NULL;
-}
-
-
-void game_set_cell_init(game g, uint x, uint y, color c);{}
-
-
-void game_set_max_moves(game g, uint nb_max_moves){
-    g.nbmax = nb_max_moves;
-}
-
-
-uint game_nb_moves_max(cgame g){
-    return g.nbmax;
-}
-
-
-color game_cell_current_color(cgame g, uint x, uint y){
-    return g.cell[x][y];
-}
-
-
-uint game_nb_moves_cur(cgame g){
-    return 0;
-}
-
-
-void game_play_one_move(game g, color c){}
-
-
-game game_copy(cgame g){
+    game g=(game)malloc(sizeof(game));
+    if (g==NULL){
+        fprintf(stderr,"errrr");
+        exit(1);
+    }
+    g->nbmax=nb_moves_max;
+    g->nbmovecur=0;
+    g->cell =(color*)malloc(SIZE*SIZE*sizeof(color));
+     g->cell_init =(color*)malloc(SIZE*SIZE*sizeof(color));
+     if (g->cell_init==NULL || g->cell==NULL){
+         fprintf(stderr,"errror\n");
+         exit(1);
+     }
+     for(int i=0;i<SIZE*SIZE;i++){
+         g->cell[i]=cells[i];
+         g->cell_init[i]=cells[i];
+     }
     return g;
 }
 
 
-void game_delete(game g){}
+game game_new_empty(){
+    game g=(game)malloc(sizeof(game));
+    if (g==NULL){
+        fprintf(stderr,"errrr\n");
+        exit(1);
+    }
+    g->nbmax=12;
+    g->nbmovecur=0;
+    g->cell =(color*)malloc(SIZE*SIZE*sizeof(color));
+     g->cell_init =(color*)malloc(SIZE*SIZE*sizeof(color));
+     if (g->cell_init==NULL || g->cell==NULL){
+         fprintf(stderr,"errror\n");
+         exit(1);
+     }
+    return g;
+}
+
+
+void game_set_cell_init(game g, uint x, uint y, color c){
+    
+    
+    }
+
+
+void game_set_max_moves(game g, uint nb_max_moves){
+    if (g==NULL || nb_max_moves==0){
+        fprintf(stderr,"not enogh memory!\n");
+        exit (EXIT_FAILURE);
+    }
+    
+    g->nbmax = nb_max_moves;
+
+}
+
+
+uint game_nb_moves_max(cgame g){
+     if (g==NULL){
+        fprintf(stderr,"not enogh memory!\n");
+        exit (EXIT_FAILURE);
+    }
+    
+    return g->nbmax;
+}
+
+
+color game_cell_current_color(cgame g, uint x, uint y){
+     if (g==NULL||x>=SIZE||y>=SIZE){
+        fprintf(stderr,"not enogh memory!\n");
+        exit (EXIT_FAILURE);
+    }
+    return  g->cell[(SIZE-y-1)*SIZE+x];
+}
+
+
+uint game_nb_moves_cur(cgame g){
+    //tester g
+    return g->nbmovecur;
+}
+
+
+void game_play_one_move(game g, color c){ return;}
+
+
+game game_copy(cgame g){
+    return NULL;
+}
+
+
+void game_delete(game g){return;}
 
 
 bool game_is_over(cgame g){
@@ -108,4 +157,4 @@ bool game_is_over(cgame g){
 }
 
 
-void game_restart(game g){}
+void game_restart(game g){return;}
