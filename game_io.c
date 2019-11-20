0b6fa0181dba9c802082de2b2cc24cca9d1d137a
lauraalanoix <laura.alanoix@etu.u-bordeaux.fr>
2019-11-20 16:42:15 +0100

    re fonction laura

---
+++ game_io.c
@@ -0,0 +1,14 @@
+#ifndef GAME_IO_H
+#define GAME_IO_H
+#include <stdio.h>
+#include "game.h"
+
+
+game game_load(char *filename){
+    return NULL;
+}
+
+
+void game_save(cgame g, char *filename){}
+
+#endif  
