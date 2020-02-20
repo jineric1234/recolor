b0e303e08353e86741622d1c11da8ed4be2569e7
lauraalanoix <laura.alanoix@etu.u-bordeaux.fr>
2020-02-20 15:22:14 +0100

    debut solv

--- recolor_solve.c
+++ recolor_solve.c
@@ -3,8 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "game_io.c"
+#include "game_io.h"
+#include "game.h"
+#include "game.c"
 
 
-void main (){
-    return EXIT_SUCCESS;
+void FIND_ONE (cgame g, char *filename ) {
+    FILE * f = fopen(filename,"w");
+    fprintf(f,"NO SOLUTION\n");
+}
+
+void NB_SOL (cgame g, char *filename) {
+  FILE * f = fopen(filename,"w");
+    fprintf(f,"NO SOLUTION\n");
+}
+
+void FIND_MIN (cgame g, char*filename) {
+  FILE * f = fopen(filename,"w");
+    fprintf(f,"NO SOLUTION\n");
+}
+
+
+int main(int argc, char *argv[]){
+    if(argc==4){
+        if (strcmp("FIND_ONE", argv[1]) == 0){
+            game g = game_load(argv[2]);
+            char *sol= strcat (argv[3],".sol");
+            FIND_ONE(g,sol);
+        }
+        if (strcmp("NB_SOL", argv[1]) == 0){
+            game g = game_load(argv[2]);
+            char *sol= strcat (argv[3],".nbsol");
+           NB_SOL(g,sol);
+        }
+        if (strcmp("FIND_MIN", argv[1]) == 0){
+            game g = game_load(argv[2]);
+            char *sol= strcat (argv[3],".sol");
+            FIND_MIN(g,sol);
+        }
+    }
+  
 }
