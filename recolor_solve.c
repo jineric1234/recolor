081219071865d9430c9330c4d4c365b42acc4a5a
anouamani <ayoub.nouamani@etu.u-bordeaux.fr>
2020-03-01 16:46:31 +0100

    solver 1.0

--- recolor_solve.c
+++ recolor_solve.c
@@ -9,8 +9,12 @@
 #include "game.c"
 
 void write_solution(char *filename,int *tab, int size_tab){
+    if (tab==NULL || filename==NULL){
+        fprintf(stderr, "erreur pointeur null\n");
+        exit(EXIT_FAILURE);
+    }
     FILE * f = fopen(filename,"w+");
-    for(uint i = 0; i<=size_tab; i++){
+    for(uint i = 0; i<size_tab; i++){
         uint v = tab[i];
         if (v>=0 && v<=9){fprintf(f,"%u ",v);}
         if (v==10){fprintf(f,"A ");}
@@ -25,7 +29,7 @@ void write_solution(char *filename,int *tab, int size_tab){
 }
 
 int* tab_move(int x) {
-    int *tab= calloc(x,sizeof(int));
+    int *tab= (int*)malloc(x * sizeof(int));
     if (tab==NULL){
         fprintf(stderr,"Error: solver tab Null\n");
         exit(EXIT_FAILURE);
@@ -33,47 +37,48 @@ int* tab_move(int x) {
     return tab;
 }
 
-int* tab_bool(int x) {
-    int *tab= malloc(sizeof(bool)*x);
+bool* tab_bool1(int x) {
+    bool *tab= (bool*)malloc(sizeof(bool) * x);
     if (tab==NULL){
         fprintf(stderr,"Error: solver tab Null\n");
         exit(EXIT_FAILURE);
     }
-    for (uint i=1; i<x; i++){
+    for (uint i=0; i<x; i++){
         tab[i]=false;
     }
     return tab;
 }
 
-int* near_move(game g){
+bool* near_move(game g){
     if(g==NULL){
         fprintf(stderr,"game NULL\n");
         exit(EXIT_FAILURE);
     }
-    int *tab = tab_bool(4);
+    bool *tab = tab_bool1(4);
     uint height = g->height;
     uint width = g->width;
     for (uint i=0; i<(height * width); i++){
         uint x = i%width;
         uint y = i/width;
         if (g->tab_bool[i]==true){
+            // upper cell : (x,y-1)
             if (y >= 1 && !g->tab_bool[i-width]) {
-                uint c = g->cell[(y - 1) * game_width(g) + x];
+                uint c = g->cell[(y - 1) * width + x];
                 tab[c]=true;
             }
             // lower cell : (x,y+1)
-            if (y + 1 < game_height(g) && !g->tab_bool[i+width]) {
-                uint c = g->cell[(y + 1) * game_width(g) + x];
+            if (y + 1 < height && !g->tab_bool[i+width]) {
+                uint c = g->cell[(y + 1) * width + x];
                 tab[c] = true;
             }
             // left cell : (x-1,y)
             if (x >= 1 && !g->tab_bool[i-1]) {
-                uint c = g->cell[y * game_width(g) + (x - 1)];
+                uint c = g->cell[y * width + (x - 1)];
                 tab[c] = true;
             }
             // right cell : (x+1,y)
-            if (x + 1 < game_width(g) && !g->tab_bool[i+1]) {
-                uint c = g->cell[y * game_width(g) + (x + 1)];
+            if (x + 1 < width && !g->tab_bool[i+1]) {
+                uint c = g->cell[y * width + (x + 1)];
                 tab[c] = true;
             }
 
@@ -83,22 +88,22 @@ int* near_move(game g){
                 // upper cell : (x,game_height(g)-1) <=> (game_height(g)-1)*game_width(g)+x)
                 if (y == 0 && !g->tab_bool[(height - 1) * width + x]) {
                 // color the cell and call function on its neighbors
-                    uint c = g->cell[(game_height(g) - 1) * game_width(g) + x];
+                    uint c = g->cell[(height - 1) * width + x];
                     tab[c] = true;
                 }
                 // lower cell : (x,0) <=> x
-                if ((y == game_height(g) - 1) && !g->tab_bool[x]) {
+                if ((y == height - 1) && !g->tab_bool[x]) {
                     uint c = g->cell[x];
                     tab[c] = true;
                 }
                 // left cell : (game_width(g)-1,y) <=> y*game_width(g)+game_width(g)-1
                 if (x == 0 && !g->tab_bool[i+width-1]) {
-                    uint c = g->cell[y * game_width(g) + game_width(g) - 1];
+                    uint c = g->cell[y * width + width - 1];
                     tab[c] = true;
                 }
                 // right cell : (0,y) <=> y*game_width(g)
-                if (x == game_width(g) - 1 && !g->tab_bool[i-width+1]) {
-                    uint c = g->cell[y * game_width(g)];
+                if (x == width - 1 && !g->tab_bool[i-width+1]) {
+                    uint c = g->cell[y * width];
                     tab[c] = true;
                 }
             }
@@ -109,29 +114,34 @@ int* near_move(game g){
 
 
 void solve_aux(game g, uint x, int *tabmove, char *filename ){
-    int *tab = near_move(g);
+    bool *tab = near_move(g);
     if (tab==NULL){
         fprintf(stderr,"tab NULL\n");
         exit(EXIT_FAILURE);
     }
+    /*
+    if (x > (g->nbmax)){
+        game_delete(g);
+        free(tab);
+        return;
+    }*/
     if (game_is_over(g)){
         write_solution(filename, tabmove, x);
         game_delete(g);
         free(tabmove);
+        free(tab);
         return;
-    }
-    if (g->nbmovecur > g->nbmax){
-        game_delete(g);
-        free(tabmove);
-        return;
-    }
-    for (uint i = 0; i<=16; i++){
-        if (tab[i]==true){
-            game copy =  game_copy(g);
-            game_play_one_move(copy,i);
-            tabmove[x]=i;
-            solve_aux(copy, x+1, tabmove, filename);
+    }else{
+        for (uint i = 0; i<4; i++){
+            if (tab[i]==true){
+                game copy =  game_copy(g);
+                game_play_one_move(copy,i);
+                tabmove[x]=i;
+                solve_aux(copy, x+1, tabmove, filename);
+            }
         }
+    game_delete(g);
+    free(tab);
     }
 }
 
