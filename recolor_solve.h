3bd8c46459d0aae2de396d6003d93a4bf0855c3f
anouamani <ayoub.nouamani@etu.u-bordeaux.fr>
2020-03-02 23:39:00 +0100

    rs.h

--- recolor_solve.h
+++ recolor_solve.h
@@ -17,7 +17,29 @@
  * @param tab
  * @param size_tab the size of the tab
  **/
-void write_solution(char *filename,int *tab, int size_tab){
+void write_solution(char *filename,int *tab, int size_tab);
+
+
+/**
+ * @brief Write the solution of the game
+ * @param filename
+ * @param tab
+ * @param size_tab the size of the tab
+ * @param x
+ * @param a
+ **/
+void write_solution1(FILE *f,int *tab, int size_tab, int *x, int *a)
+
+/**
+ * @brief Write the solution of the game
+ * @param filename
+ * @param tab
+ * @param size_tab the size of the tab
+ * @param x
+ * @param a
+ * @param m
+ **/
+void write_solution2(FILE *f,int *tab, int size_tab, int *v, int *a, int *m)
 
 
 /**
@@ -31,7 +53,7 @@ int* tab_move(int x);
  * @brief true if..
  * @param x 
  **/
-bool* tab_bool1(int x)
+bool* tab_bool1(int x);
 
 
 /**
@@ -53,6 +75,42 @@ bool* near_move(game g);
  **/
 void solve_aux(game g, uint x, int *tabmove, char *filename );
 
+/**
+ * @brief Solve the game 
+ * @param g
+ * @param x
+ * @param tabmove 
+ * @param filename 
+ * @param z
+ * @param V
+
+ **/
+void solve_aux(game g, uint x, int *tabmove, FILE *filename, uint z, int *v);
+
+/**
+ * @brief Solve the game 
+ * @param g
+ * @param x
+ * @param tabmove 
+ * @param filename 
+ * @param z
+ * @param v
+ * @param a
+ **/
+void solve_aux1(game g, uint x, int *tabmove, FILE *filename, uint z, int *v, int *a);
+
+/**
+ * @brief Solve the game 
+ * @param g
+ * @param x
+ * @param tabmove 
+ * @param filename 
+ * @param z
+ * @param v
+ * @param a
+ * @param m
+ **/
+void solve_aux2(game g, uint x, int *tabmove, FILE *filename, uint z, int *v, int*a, int *m);
 
 /**
  * @brief allows to calculate a solution (if it exists) to the problem stored in the file
